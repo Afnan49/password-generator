8abb37c7ce63c383581fd085b1a85f052ed3cf97
Afnan49 <hyaty0764@gmail.com>
2024-06-05 02:30:59 +0300

    create an random password generator application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'PW';
+  //==================================< properties >=====================================
+  length: number = 0;
+  includingLetter: boolean = false;
+  includingNumber: boolean = false;
+  includingSymbols: boolean = false;
+  passward: string = '';
+  //===============================< change passwerd length >===============================
+  changePasswordLength(value: any) {
+    if (value) {
+      if (!isNaN(parseInt(value.target.value))) {
+        this.length = parseInt(value.target.value);
+      }
+    }
+  }
+  //=====================< including letters to the password >============================
+  onChangeLetter() {
+    this.includingLetter = !this.includingLetter;
+  }
+  //=====================< including numbers to the password >===========================
+  onChangeNumber() {
+    this.includingNumber = !this.includingNumber;
+  }
+  //=====================< including symbols to the password >===========================
+  onChangeSymbols() {
+    this.includingSymbols = !this.includingSymbols;
+  }
+  //=============================< generating password >=================================
+  onButtonClick() {
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFJHIGKLMNOBQRSTUVWXYZ';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*';
+    let validValue = '';
+    if (this.includingLetter) {
+      validValue += letters;
+    }
+    if (this.includingNumber) {
+      validValue += numbers;
+    }
+    if (this.includingSymbols) {
+      validValue += symbols;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      let index = Math.floor(Math.random() * validValue.length);
+      generatedPassword += validValue[index];
+    }
+    this.passward = generatedPassword;
+  }
 }
